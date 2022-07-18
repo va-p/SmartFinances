aaf6c2f4b34f6bb711ceb64e6d68c7ef567710f1
va-p <paiva.v@icloud.com>
2022-07-18 09:08:05 -0300

    feat :sparkles:(lists): added divider and list separator component

---
+++ src/components/Divider/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import {
+  Container,
+  Line
+} from './styles';
+
+
+export function Divider() {
+  return (
+    <Container>
+      <Line />
+    </Container>
+  );
+}
---
+++ src/components/Divider/styles.ts
@@ -0,0 +1,12 @@
+import styled from 'styled-components/native';
+
+export const Container = styled.View`
+  flex: 1;
+`;
+
+export const Line = styled.View`
+  width: 100%;
+  height: 1px;
+  margin: 12px 0;
+  background-color: ${({ theme }) => theme.colors.secondary};
+`;
