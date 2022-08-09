0a71a49e82a331b6b9a934f2e343e833bf63aa52
VaP <paiva.v@icloud.com>
2022-08-09 18:49:43 -0300

    style :nail_care:(options menu screen): change background color

--- src/screens/OptionsMenu/styles.ts
+++ src/screens/OptionsMenu/styles.ts
@@ -3,7 +3,8 @@ import styled from 'styled-components/native';
 import { RFValue } from 'react-native-responsive-fontsize';
 
 export const Container = styled.View`
-  flex: 1;  
+  flex: 1;
+  background-color: ${({ theme }) => theme.colors.background};
 `;
 
 export const ContentScroll = styled.ScrollView``;
