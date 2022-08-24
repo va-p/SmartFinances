4dfe29230484138fa96d3523e8726b46dad7c6f1
VaP <paiva.v@icloud.com>
2022-08-24 15:05:40 -0300

    chore :recycle:: added new resources for icon and splash screen

--- src/utils/currencies.ts
+++ src/utils/currencies.ts
@@ -1,4 +1,6 @@
 export const currencies = [
-  { name: 'BRL', simbol: 'R$'},
-  { name: 'BTC', simbol: '₿' },
+  { id: '1', name: 'Bitcoin', code: 'BTC', symbol: '₿'},
+  { id: '2', name: 'Dólar Americano', code: 'USD', symbol: 'US$'},
+  { id: '3', name: 'Euro', code: 'EUR', symbol: '€'},
+  { id: '4', name: 'Real Brasileiro', code: 'BRL', symbol: 'R$'}
 ];
