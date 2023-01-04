b28808743d3c37ba7990d2496753fd8fd6607e93
VaP <paiva.v@icloud.com>
2023-01-04 16:22:21 -0300

    build :hammer_and_wrench: (category): refactor category list item

--- src/components/CategoryListItem/index.tsx
+++ src/components/CategoryListItem/index.tsx
@@ -1,16 +1,12 @@
 import React from 'react';
-import { Animated } from 'react-native';
 import {
   Container,
   Category,
   Icon,
-  Name,
-  DeleteButton,
-  DeleteButtonIcon,
-  DeleteButtonText
+  Name
 } from './styles';
 
-import { RectButtonProps, Swipeable } from 'react-native-gesture-handler';
+import { RectButtonProps } from 'react-native-gesture-handler';
 
 export interface IconProps {
   id: string;
@@ -34,46 +30,18 @@ export interface CategoryProps {
 
 type Props = RectButtonProps & {
   data: CategoryProps;
-  onSwipeableLeftOpen?: () => void;
 }
 
 export function CategoryListItem({
   data,
-  onSwipeableLeftOpen,
   ...rest
 }: Props) {
-  function handleCategorySwipeLeft(
-    progressAnimatedValue: Animated.AnimatedInterpolation,
-    dragAnimatedValue: Animated.AnimatedInterpolation
-  ) {
-    const delay = progressAnimatedValue.interpolate({
-      inputRange: [0, 1],
-      outputRange: [0, 1]
-    });
-    const opacity = dragAnimatedValue.interpolate({
-      inputRange: [-150, -50],
-      outputRange: [1, 0],
-      extrapolate: 'clamp',
-    });
-    return (
-      <DeleteButton style={{ opacity }}>
-        <DeleteButtonText>Excluir</DeleteButtonText>
-        <DeleteButtonIcon name='trash-outline' />
-      </DeleteButton>
-    )
-  };
-
   return (
     <Container>
-      <Swipeable
-        renderRightActions={handleCategorySwipeLeft}
-        onSwipeableOpen={onSwipeableLeftOpen}
-      >
-        <Category {...rest} icon={data.icon.name} color={data.color.hex}>
-          <Icon name={data.icon.name} color={data.color.hex} />
-          <Name>{data.name}</Name>
-        </Category>
-      </Swipeable>
+      <Category {...rest} icon={data.icon.name} color={data.color.hex}>
+        <Icon name={data.icon.name} color={data.color.hex} />
+        <Name>{data.name}</Name>
+      </Category>
     </Container >
   );
 }
--- src/components/CategoryListItem/styles.ts
+++ src/components/CategoryListItem/styles.ts
@@ -1,11 +1,11 @@
-import { Animated } from 'react-native';
 import styled from 'styled-components/native';
 
 import { RFValue } from 'react-native-responsive-fontsize';
 import { Ionicons } from '@expo/vector-icons';
 
-type ColorProps = {
+type Props = {
   color: string;
+  icon: string;
 }
 
 export const Container = styled.View`
@@ -14,7 +14,7 @@ export const Container = styled.View`
 
 export const Category = styled.TouchableOpacity.attrs({
   activeOpacity: 0.6
-}) <ColorProps>`
+}) <Props>`
   width: 100%;
   min-height: 56px;
   max-height: 56px;
@@ -28,7 +28,7 @@ export const Category = styled.TouchableOpacity.attrs({
   border-radius: 10px;
 `;
 
-export const Icon = styled(Ionicons) <ColorProps>`
+export const Icon = styled(Ionicons) <Props>`
   font-size: ${RFValue(20)}px;
   margin-right: 8px;
   color: ${({ color }) => color};
@@ -38,24 +38,4 @@ export const Name = styled.Text`
   font-family: ${({ theme }) => theme.fonts.regular};
   font-size: ${RFValue(14)}px;
   color: ${({ theme }) => theme.colors.text};
-`;
-
-export const DeleteButton = styled(Animated.View)`
-  flex: 1;
-  flex-direction: row;
-  align-items: center;
-  justify-content: center;
-  padding-left: 70%;
-  margin-bottom: 10px;
-  background-color: ${({ theme }) => theme.colors.attention};
-  border-radius: 10px;
-`;
-
-export const DeleteButtonIcon = styled(Ionicons)`
-  font-size: ${RFValue(20)}px;
-  color: ${({ theme }) => theme.colors.shape};
-`;
-
-export const DeleteButtonText = styled.Text`
-  color: ${({ theme }) => theme.colors.shape};
 `;
