12de8d8eb868ebcc95482e40287c3782049b8689
Vitor Paiva <paiva.v@icloud.com>
2023-01-29 17:58:52 -0300

    refactor :package:(refactor): refactory code

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,8 +1,7 @@
 import AsyncStorage from '@react-native-async-storage/async-storage';
+import axios from 'axios';
 
 import { COLLECTION_TOKENS } from '@configs/database';
-
-const axios = require('axios').default;
 
 const api = axios.create({
   baseURL: 'https://x6if-pd9g-tkt7.n7.xano.io/api:ckiy-sBf'
