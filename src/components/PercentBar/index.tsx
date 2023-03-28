2b2ef0acedd543a0091f18f3656b2b6adf1d0468
Vitor <paiva.v@icloud.com>
2023-03-27 23:36:37 -0300

    style :nail_care:(budgets): creates budget animation

--- src/components/PercentBar/index.tsx
+++ src/components/PercentBar/index.tsx
@@ -1,5 +1,11 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Container, Percent, Percentage } from './styles';
+
+import {
+  useAnimatedStyle,
+  useSharedValue,
+  withTiming,
+} from 'react-native-reanimated';
 
 interface Props {
   is_amount_reached: boolean;
@@ -7,12 +13,20 @@ interface Props {
 }
 
 export function PercentBar({ is_amount_reached, percentage }: Props) {
+  const sharedProgress = useSharedValue(percentage);
+  const styledAnimated = useAnimatedStyle(() => {
+    return {
+      width: `${sharedProgress.value}%`,
+    };
+  });
+
+  useEffect(() => {
+    sharedProgress.value = withTiming(percentage, { duration: 1000 });
+  }, [percentage]);
+
   return (
     <Container>
-      <Percentage
-        is_amount_reached={is_amount_reached}
-        style={{ width: `${percentage}` }}
-      >
+      <Percentage is_amount_reached={is_amount_reached} style={styledAnimated}>
         <Percent>{percentage}</Percent>
       </Percentage>
     </Container>
