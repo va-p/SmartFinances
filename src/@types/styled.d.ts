b8105d28392f1d24d2dded2249a42d5536c67b48
Vitor <paiva.v@icloud.com>
2023-04-05 10:49:39 -0300

    refactor :package:(refactor): refactory code

--- src/@types/styled.d.ts
+++ src/@types/styled.d.ts
@@ -1,8 +1,9 @@
 import 'styled-components';
+
 import theme from '@themes/theme';
 
 declare module 'styled-components' {
-  type ThemeType = typeof theme
+  type ThemeType = typeof theme;
 
   export interface DefaultTheme extends ThemeType {}
 }
--- src/global/themes/theme.ts
+++ src/global/themes/theme.ts
@@ -25,7 +25,6 @@ export default {
   fonts: {
     regular: 'Poppins_400Regular',
     medium: 'Poppins_500Medium',
-    bold: 'Poppins_700Bold'
-  }
-
-}
+    bold: 'Poppins_700Bold',
+  },
+};
