799eb1368a4d8a8ed35d4a36f4d90c14ddac85f7
va-p <paiva.v@icloud.com>
2025-03-13 20:35:40 -0300

    fix: removes logs

--- src/components/PeriodRuler/styles.ts
+++ src/components/PeriodRuler/styles.ts
@@ -9,8 +9,6 @@ export const Container = styled.View<ContainerProps>`
   flex-direction: row;
   align-items: center;
   padding: ${({ horizontalPadding }) => `0 ${horizontalPadding}px`};
-  // padding-right: ${({ horizontalPadding }) => horizontalPadding}px;
-  // padding-left: ${({ horizontalPadding }) => horizontalPadding}px;
 `;
 
 export const MonthSelectButton = styled(BorderlessButton)``;
