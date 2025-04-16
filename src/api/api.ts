f83b72266c061fb4e5d714c271e13281895b78d5
va-p <paiva.v@icloud.com>
2025-04-16 20:56:12 -0300

    fix: adds types to api instance

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,17 +1,17 @@
-import axios from 'axios';
+import axios, { AxiosInstance, AxiosRequestConfig } from 'axios';
 
 import { storageToken } from '@database/database';
 
-const api = axios.create({
+const api: AxiosInstance = axios.create({
   baseURL: 'https://xjg3-npzd-66ef.b2.xano.io/api:ckiy-sBf',
 });
 
-api.interceptors.request.use(async (config: any) => {
+api.interceptors.request.use(async (config: AxiosRequestConfig) => {
   try {
     const jsonToken = storageToken.getString('token');
     if (jsonToken) {
       const loggedInUserAuthToken = JSON.parse(jsonToken);
-      config.headers.Authorization = `Bearer ${loggedInUserAuthToken}`;
+      config.headers!.Authorization = `Bearer ${loggedInUserAuthToken}`;
     }
   } catch (error) {
     console.error('api error =>', error);
