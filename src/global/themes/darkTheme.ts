53a505fd392a8a4b75792ac4ea871892258e8318
va-p <paiva.v@icloud.com>
2025-09-05 19:04:36 -0300

    feat: adds new theme dark

--- src/global/themes/darkTheme.ts
+++ src/global/themes/darkTheme.ts
@@ -1,31 +1,36 @@
 export default {
   colors: {
-    primary: 'rgb(249, 150, 0)',
-    primaryLight: 'rgba(255, 82, 0, 0.3)',
+    primary: 'rgb(255, 170, 41)',
+    primary_light: 'rgba(255, 82, 0, 0.5)',
 
-    background: 'rgb(230, 233, 244)',
-    backgroundCardHeader: 'rgba(255, 255, 255, 0.25)',
+    background: 'rgb(0, 0, 0)',
+    backgroundNav: 'rgba(0, 0, 0, 0.95)',
+    backgroundCardHeader: 'rgba(20, 20, 20, 0.7)',
 
-    shape: 'rgb(255, 255, 255)',
+    gradientStart: 'rgb(20, 20, 20)',
+    gradientEnd: 'rgb(0, 0, 0)',
 
-    button: '#FFAA29',
+    shape: 'rgba(40, 40, 40, 0.8)',
+    shapeDark: 'rgb(120, 120, 120)',
 
-    title: 'rgb(133, 201, 240)',
-    text: '#000000',
-    textLight: '#FFFFFF',
-    textPlaceholder: '#969CB2',
+    button: 'rgb(255, 170, 41)',
 
-    border: '#707070',
+    title: 'rgb(255, 255, 255)',
+    text: 'rgb(220, 220, 220)',
+    textPlaceholder: 'rgb(100, 100, 100)',
+    textLight: 'rgb(0, 0, 0)',
+
+    border: 'rgb(60, 60, 60)',
 
     success: 'rgb(18, 195, 140)',
-    successLight: 'rgba(18, 195, 140, 0.7)',
+    success_light: 'rgba(18, 195, 140, 0.5)',
 
     attention: 'rgb(251, 103, 103)',
-    attentionLight: 'rgba(251, 103, 103, 0.7)',
+    attention_light: 'rgba(251, 103, 103, 0.5)',
 
-    overlay: 'rgba(0, 0, 0, 0.7)',
-    overlayLight: 'rgba(0, 0, 0, 0.1)',
-    overlayMedium: 'rgba(150, 156, 178, 0.1)',
+    overlay: 'rgba(255, 255, 255, 0.1)',
+    overlay10: 'rgba(255, 255, 255, 0.05)',
+    overlayGray: 'rgba(100, 100, 100, 0.1)',
   },
 
   fonts: {
