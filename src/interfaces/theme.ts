ff49992761c1f5b2f840a3396ed872ac3edc1f30
va-p <paiva.v@icloud.com>
2025-09-07 10:29:30 -0300

    feat: adds types to theme

---
+++ src/interfaces/theme.ts
@@ -0,0 +1,44 @@
+type Colors = {
+  primary: string;
+  primary_light: string;
+
+  background: string;
+  backgroundNav: string;
+  backgroundCardHeader: string;
+
+  gradientStart: string;
+  gradientEnd: string;
+
+  shape: string;
+  shapeDark: string;
+
+  button: string;
+
+  title: string;
+  text: string;
+  textPlaceholder: string;
+  textLight: string;
+
+  border: string;
+
+  success: string;
+  success_light: string;
+
+  attention: string;
+  attention_light: string;
+
+  overlay: string;
+  overlay10: string;
+  overlayGray: string;
+};
+
+type fonts = {
+  regular: string;
+  medium: string;
+  bold: string;
+};
+
+export interface ThemeProps {
+  colors: Colors;
+  fonts: fonts;
+}
