c034242fb592e402b876431d728873a3645c3e62
va-p <paiva.v@icloud.com>
2026-08-09 11:21:20 -0300

    feat(institutions): T15 — InstitutionListItem component

---
+++ src/components/InstitutionListItem/styles.ts
@@ -0,0 +1,26 @@
+import styled from 'styled-components/native';
+
+import { RFValue } from 'react-native-responsive-fontsize';
+import { RectButton } from 'react-native-gesture-handler';
+import Animated from 'react-native-reanimated';
+
+const RectButtonAnimated = Animated.createAnimatedComponent(RectButton);
+
+export const Container = styled(RectButtonAnimated)`
+  flex: 1;
+  min-height: 56px;
+  max-height: 56px;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+  padding: 16px;
+  margin-bottom: 8px;
+  background-color: ${({ theme }) => theme.colors.shape};
+  border-radius: 10px;
+`;
+
+export const Name = styled.Text`
+  font-family: ${({ theme }) => theme.fonts.regular};
+  font-size: ${RFValue(12)}px;
+  color: ${({ theme }) => theme.colors.title};
+`;
